3fcfa5605e5e19780883d2c60699c69fa1885806
lsbu-electronics <126675512+LSBU-Electronics-Lab@users.noreply.github.com>
2023-08-14 15:38:42 +0100

    Add files via upload

---
+++ LAB-2/variables.py
@@ -0,0 +1,14 @@
+from niryo_one_tcp_client import *
+
+
+gripper_speed = 1000 # 1000 max
+gripper_used = RobotTool.GRIPPER_1  # Tool used for picking
+# Predefined safe Coordinates
+sleep_joints = [0.0, 0.55, -1.2, 0.0, 0.0, 0.0]
+counter = 0
+J1 = -1.5
+J2 = 0.0
+J3 = 0.0
+J4 = 0.0
+J5 = 0.0
+J6 = 0.0
